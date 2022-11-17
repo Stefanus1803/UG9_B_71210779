5fed43dc8416badc0d4a0de7a435826282a1254e
Stefanus <93358497+Stefanus1803@users.noreply.github.com>
2022-11-17 14:00:42 +0700

    Add files via upload
    Mengupload file lewat github

---
+++ 1_A_71210779.py
@@ -0,0 +1,3 @@
+diameter = int(input("Masukan Diameter (Meter) : "))
+penjumlahan = diameter * 3.14
+print("Jarak yang harus di tempuh: ", penjumlahan, 'Meter')
---
+++ 2_A_71210779.py
@@ -0,0 +1,3 @@
+angka = int(input("Masukkan Angka yang ingin di Pangkatkan: \n>>"))
+print()
+print("Angka anda telah di pangkat kan 7! anda mendapat angka: ", angka **7)
